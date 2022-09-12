96bf9c6475bee33a47deb894ad023dff70d2c74e
choyweiqiang <73052459+choyweiqiang@users.noreply.github.com>
2022-09-13 01:01:40 +0800

    feat: create date component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+import { DateComponent } from "../../components/DateComponent";
+
 export const Home = () => {
-  return <div className="page h-full"></div>;
+  const [dateChange, setDateChange] = useState(
+    new Date().toLocaleDateString("en-ca")
+  );
+
+  const [timeChange, setTimeChange] = useState(new Date().toLocaleTimeString());
+
+  return (
+    <div className="page h-full">
+      <div className="flex justify-center mt-8">
+        <DateComponent
+          onDateChange={[dateChange, setDateChange]}
+          onTimeChange={[timeChange, setTimeChange]}
+        />
+      </div>
+    </div>
+  );
 };
