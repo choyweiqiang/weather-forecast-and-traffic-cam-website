ab9b0110c2cdb37b1d1a6af01181de698f154ed1
Choy Wei Qiang <73052459+choyweiqiang@users.noreply.github.com>
2022-09-13 01:02:57 +0800

    feat: create date component (#2)

---
+++ src/components/DateComponent/index.tsx
@@ -0,0 +1,40 @@
+export const DateComponent = ({
+  onDateChange,
+  onTimeChange,
+}: {
+  onDateChange: any;
+  onTimeChange: any;
+}) => {
+  const handleDateChange = (e: any) => {
+    onDateChange[1](e.target.value);
+  };
+
+  const handleTimeChange = (e: any) => {
+    onTimeChange[1](e.target.value);
+  };
+
+  return (
+    <div className="flex flex-row gap-4">
+      <div>
+        <input
+          type="date"
+          id="date"
+          value={onDateChange[0]}
+          max={new Date().toLocaleDateString("en-ca")}
+          onChange={(e) => handleDateChange(e)}
+          className="p-2 rounded-lg border drop-shadow-md text-primary"
+        />
+      </div>
+
+      <div>
+        <input
+          type="time"
+          id="time"
+          value={onTimeChange[0]}
+          onChange={(e) => handleTimeChange(e)}
+          className="p-2 rounded-lg border drop-shadow-md text-primary w-fit"
+        />
+      </div>
+    </div>
+  );
+};
