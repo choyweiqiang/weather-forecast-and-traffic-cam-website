df4625a634c190a761e616237bca62425e5d0fdc
choyweiqiang <73052459+choyweiqiang@users.noreply.github.com>
2022-09-12 22:26:22 +0800

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+export const Home = () => {
+  return (
+    <div className="page h-full">
+      <h1 className="m-40">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi maxime
+        delectus quibusdam fugiat libero consequatur magnam, accusamus nam vero
+        ea!
+      </h1>
+      <h1 className="m-40">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi maxime
+        delectus quibusdam fugiat libero consequatur magnam, accusamus nam vero
+        ea!
+      </h1>
+      <h1 className="m-40">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi maxime
+        delectus quibusdam fugiat libero consequatur magnam, accusamus nam vero
+        ea!
+      </h1>
+      <h1 className="m-40">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi maxime
+        delectus quibusdam fugiat libero consequatur magnam, accusamus nam vero
+        ea!
+      </h1>
+    </div>
+  );
+};
