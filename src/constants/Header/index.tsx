676fc52d9977b0630ed4d99d780be5cfb0ba2309
choyweiqiang <73052459+choyweiqiang@users.noreply.github.com>
2022-09-13 21:26:28 +0800

    style: mobile responsiveness

--- src/constants/Header/index.tsx
+++ src/constants/Header/index.tsx
@@ -1,5 +1,5 @@
 export const Header = () => (
-  <nav className="sticky top-0 flex justify-center items-center w-full bg-white h-20 drop-shadow-xl">
+  <nav className="sticky top-0 flex justify-center items-center w-full bg-white h-20 drop-shadow-xl z-50">
     <div className="text-primary">
       Weather Forecast {"&"} Traffic Cam Website
     </div>
